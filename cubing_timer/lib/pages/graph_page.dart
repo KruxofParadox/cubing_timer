72941877997edd8ac102a2ea312b667931eb74fc
Spencer Shore <t.spencer.shore@gmail.com>
2023-03-28 12:56:57 -0400

    Simple graph layout -> need to add cards for times

--- cubing_timer/lib/pages/graph_page.dart
+++ cubing_timer/lib/pages/graph_page.dart
@@ -1,3 +1,4 @@
+import 'package:cubing_timer/components/graph_components/no_graph_widget.dart';
 import 'package:flutter/material.dart';
 
 class GraphPage extends StatelessWidget {
@@ -5,8 +6,21 @@ class GraphPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Graph Page'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const NoGraphDisplay(),
+          ),
+          const SizedBox(height: 50),
+
+          /// TODO:
+          /// Replace below with Listview.builder -> display individual solves with slidable widget
+          /// slide on side to delete
+          const Text('No Solve Times Yet'),
+        ],
+      ),
     );
   }
 }
