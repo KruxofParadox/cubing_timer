0dd3f0e2e03cdf99d865c6e9dcb35d69dba22309
Spencer Shore <t.spencer.shore@gmail.com>
2023-04-08 18:15:53 -0400

    Fixed issue from previous commit

--- cubing_timer/lib/pages/settings_page.dart
+++ cubing_timer/lib/pages/settings_page.dart
@@ -10,71 +10,67 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (settings_index == 0) {
-      return SizedBox.expand(
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-            Container(
-              width: 350,
-              height: 620,
-              decoration: BoxDecoration(
-                color: baseGrey,
-                borderRadius: BorderRadius.circular(12),
-              ),
+    return SizedBox.expand(
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          Container(
+            width: 350,
+            height: 620,
+            decoration: BoxDecoration(
+              color: baseGrey,
+              borderRadius: BorderRadius.circular(12),
+            ),
 
-              // INNER SETTINGS PANE
-              child: Column(
-                children: [
-                  const SizedBox(height: 10),
+            // INNER SETTINGS PANE
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
 
-                  // on tap -> instead display general settings page
-                  SettingsTile(
-                    text: 'General',
-                    index: settings_index,
-                  ),
-                  const Divider(color: Colors.black),
-                  SettingsTile(
-                    text: 'Appearance',
-                    index: settings_index,
-                  ),
-                  const Divider(color: Colors.black),
-                  SettingsTile(
-                    text: 'My Portfolio',
-                    index: settings_index,
-                  ),
-                  const Divider(color: Colors.black),
-                  SettingsTile(
-                    text: 'Github',
-                    index: settings_index,
-                  ),
-                  const SizedBox(height: 40),
+                // on tap -> instead display general settings page
+                SettingsTile(
+                  text: 'General',
+                  index: settings_index,
+                ),
+                const Divider(color: Colors.black),
+                SettingsTile(
+                  text: 'Appearance',
+                  index: settings_index,
+                ),
+                const Divider(color: Colors.black),
+                SettingsTile(
+                  text: 'My Portfolio',
+                  index: settings_index,
+                ),
+                const Divider(color: Colors.black),
+                SettingsTile(
+                  text: 'Github',
+                  index: settings_index,
+                ),
+                const SizedBox(height: 40),
 
-                  // IMAGE FOR APP
-                  Container(
-                    width: 250,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Add image here',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+                // IMAGE FOR APP
+                Container(
+                  width: 250,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Add image here',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
-    } else {
-      return Container();
-    }
+          ),
+        ],
+      ),
+    );
   }
 }
