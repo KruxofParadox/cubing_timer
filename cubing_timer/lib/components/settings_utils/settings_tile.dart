44314a72e236c0874e4c92be5a231ace031b03c5
Spencer Shore <t.spencer.shore@gmail.com>
2023-05-09 21:54:12 -0400

    Fixed version issues

--- cubing_timer/lib/components/settings_utils/settings_tile.dart
+++ cubing_timer/lib/components/settings_utils/settings_tile.dart
@@ -3,44 +3,60 @@ import 'package:flutter/material.dart';
 
 class SettingsTile extends StatelessWidget {
   final String text;
-  int index;
+  bool subMenuOpen;
 
   SettingsTile({
     super.key,
     required this.text,
-    required this.index,
+    this.subMenuOpen = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            text,
-            style: convertFontToUbuntu(
-              20,
-              true,
-              textColor,
-            ),
-          ),
-          IconButton(
-            onPressed:
-                // direct user to settings_page for that specific setting
-                // maybe add in as an input variable?
-
-                () {
-              index++;
-            },
-            icon: const Icon(
-              Icons.arrow_forward_ios_rounded,
-              size: 17,
-            ),
-          ),
-        ],
-      ),
+      child: !subMenuOpen ? unopenedTile() : Container(),
     );
   }
+
+  Row unopenedTile() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text,
+          style: convertFontToUbuntu(
+            20,
+            true,
+            textColor,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            // direct user to settings_page for that specific setting
+            // maybe add in as an input variable?
+
+            subMenuOpen = !subMenuOpen;
+          },
+          icon: const Icon(
+            Icons.arrow_forward_ios_rounded,
+            size: 17,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column openedTile() {
+    return Column(
+      children: [
+        unopenedTile(),
+        Container(
+          child: Text('Test text'),
+        ),
+      ],
+    );
+  }
+
+  void changeMenuOpen() {}
 }
--- cubing_timer/lib/pages/settings_page.dart
+++ cubing_timer/lib/pages/settings_page.dart
@@ -4,73 +4,61 @@ import 'package:flutter/material.dart';
 import '../components/settings_utils/settings_tile.dart';
 
 class SettingsPage extends StatelessWidget {
-  SettingsPage({super.key});
-
-  int settings_index = 0;
+  const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      child: Column(
-        children: [
-          const SizedBox(height: 30),
-          Container(
-            width: 350,
-            height: 620,
-            decoration: BoxDecoration(
-              color: baseGrey,
-              borderRadius: BorderRadius.circular(12),
-            ),
-
-            // INNER SETTINGS PANE
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-
-                // on tap -> instead display general settings page
-                SettingsTile(
-                  text: 'General',
-                  index: settings_index,
-                ),
-                const Divider(color: Colors.black),
-                SettingsTile(
-                  text: 'Appearance',
-                  index: settings_index,
-                ),
-                const Divider(color: Colors.black),
-                SettingsTile(
-                  text: 'My Portfolio',
-                  index: settings_index,
-                ),
-                const Divider(color: Colors.black),
-                SettingsTile(
-                  text: 'Github',
-                  index: settings_index,
-                ),
-                const SizedBox(height: 40),
-
-                // IMAGE FOR APP
-                Container(
-                  width: 250,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Add image here',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+        child: Column(children: [
+      const SizedBox(height: 30),
+      Container(
+          width: 350,
+          height: 620,
+          decoration: BoxDecoration(
+            color: baseGrey,
+            borderRadius: BorderRadius.circular(12),
           ),
-        ],
-      ),
-    );
+
+          // INNER SETTINGS PANE
+          child: Column(children: [
+            const SizedBox(height: 10),
+
+            // on tap -> instead display general settings page
+            SettingsTile(
+              text: 'General',
+            ),
+            const Divider(color: Colors.black),
+            SettingsTile(
+              text: 'Appearance',
+            ),
+            const Divider(color: Colors.black),
+            SettingsTile(
+              text: 'My Portfolio',
+            ),
+            const Divider(color: Colors.black),
+            SettingsTile(
+              text: 'Github',
+            ),
+            const SizedBox(height: 40),
+
+            // IMAGE FOR APP
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Center(
+                child: Text(
+                  'Add image here',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ]))
+    ]));
   }
 }
